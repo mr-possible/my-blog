9e04ce8e7804c0d108166e7721f8a6bb1c346078
mr-possible <sambhavd4@gmail.com>
2022-12-13 08:53:09 +0530

    Tweaked post detail and refactored views.py

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,32 +1,24 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 from .models import Post
-
-# just added for now. Later these will be pulled from a database
-all_posts = []
-
-
-def get_date_from_posts(post):
-    return post['date']
 
 
 def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
+    latest_posts = Post.objects.all().order_by(
+        "-date")[:3]     # -date means descending order
     return render(request, 'blogapp/index.html', {
         "posts": latest_posts
     })
 
 
 def posts(request):
-    all_posts = Post.objects.all()
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, 'blogapp/all-posts.html', {
         'all_posts': all_posts
     })
 
 
 def post_detail(request, slug):
-    # best practice in python. Basically will give what we want. search and explore about this in google!
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, 'blogapp/post-detail.html', {
         'post': identified_post
     })
